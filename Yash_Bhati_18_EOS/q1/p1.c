b793e287d44170a008d5ee11e044f6d291a1885d
yashbhati01 <yash.bhati53@gmail.com>
2022-12-26 16:28:33 +0530

    Question 1

--- Yash_Bhati_18_EOS/q1/p1.c
+++ Yash_Bhati_18_EOS/q1/p1.c
@@ -12,19 +12,19 @@ int main()
     unsigned int nbytes_read, nbytes_write;
     char read_msg1[50000000], read_msg2[5000000];
 
-    pipe(fd1);
-    pipe(fd2);
+    pipe(fd1);				// creating pipe
+    pipe(fd2);				// creating pipe
 
-    id = fork();
+    id = fork();			// creating child process using fork
 
 	if(id == 0)
 	{
 		close(fd[1]);
 		close(fd[0]);
 		
-		nbytes_read = read(fd[0], execl("/usr/include/stdio.h","","-al",NULL),sizeof(execl("/usr/include/stdio.h","","-al",NULL)));
+		nbytes_read = read(fd[0],execl("/usr/include/stdio.h","","-al",NULL) ,sizeof());
 		
-		nbytes_write = write(fd[1], ,sizeof());
+		nbytes_write = write(fd[1], read_msg1[50000000],sizeof(read_msg1[50000000]));
 		
 	}
 	
